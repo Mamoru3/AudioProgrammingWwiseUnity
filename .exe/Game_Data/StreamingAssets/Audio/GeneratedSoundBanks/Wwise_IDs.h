8f5142c30195ffa705744b4e7726b2566f5fe545
1904477 <59955897+1904477@users.noreply.github.com>
2023-01-08 10:29:47 +0000

    Final application pushed

---
+++ .exe/Game_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BACKGROUNDMUSIC = 626769978U;
+        static const AkUniqueID BARSOPEN = 2329734039U;
+        static const AkUniqueID BELL = 1240512178U;
+        static const AkUniqueID BUTTONPRESS = 317641954U;
+        static const AkUniqueID CHESTLOCK = 2250817457U;
+        static const AkUniqueID CHESTOPEN = 2892697348U;
+        static const AkUniqueID COINCOLLECT = 815433836U;
+        static const AkUniqueID DOOR1UNLOCK = 4011786762U;
+        static const AkUniqueID DOOR2UNLOCK = 2971435821U;
+        static const AkUniqueID DOOR3UNLOCK = 728871448U;
+        static const AkUniqueID FIREPLACE = 1404152342U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GRUNT = 3797621065U;
+        static const AkUniqueID HARPOONSHOT = 3880051586U;
+        static const AkUniqueID HOUSEDOOROPEN = 3708219715U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID JUMPLAND = 1548130050U;
+        static const AkUniqueID OBJECTDROP = 29191587U;
+        static const AkUniqueID PICKUPCOLLECTED = 787725374U;
+        static const AkUniqueID WATERFALL = 2074477625U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID HARDSURFACE = 3826567475U;
+                static const AkUniqueID SAND = 803837735U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
